078ef1de9616cb359c06eb4128b422fbbb57f093
kakapela <kacperkapela1@gmail.com>
2018-04-28 12:50:23 +0200

    Create PrinterView and List.

---
+++ src/main/java/controller/PrinterController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import model.dao.Database;
+import model.devices.output.Printer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrinterController implements Initializable{
+
+    @FXML
+    private TextArea productListArea;
+    Printer printer =new Printer();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        productListArea.setText("TOTAL SUM: "+ printer.printTotalSum()+"\n\n");
+        productListArea.appendText(printer.printProductList());
+    }
+
+    public void exitProgram(MouseEvent event) {
+            Platform.exit();
+        Database.getInstance().disconnect();
+    }
+}
